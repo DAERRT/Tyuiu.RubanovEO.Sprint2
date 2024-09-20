c4c391e69b637ccb81cfa4aac37155a57c681e10
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-09-20 17:35:12 +0500

    Task2

---
+++ Tyuiu.RubanovEO.Sprint2.Task2.V6/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using Tyuiu.RubanovEO.Sprint2.Task2.V6.Lib;
+
+namespace Tyuiu.RubanovEO.Sprint2.Task2.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2| Выполнил: Рубанов Е. О. | ПКТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if - полная и короткая форма записи.                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Рубанов Егор Олегович | ПКТБ-24-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает целые значения с клавиатуры и   *");
+            Console.WriteLine("* вычисляет находится ли точка с координатами X,Y в заштрихованной области*");
+            Console.WriteLine("*                                                                         *");
+            while (true) 
+            {
+                Console.WriteLine("***************************************************************************");
+                Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+                Console.WriteLine("***************************************************************************");
+                Console.WriteLine("Введите x:");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите y:");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("***************************************************************************");
+                Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+                Console.WriteLine("***************************************************************************");
+                Console.WriteLine(ds.CheckDotInShadedArea(x, y));
+            }
+        }
+    }
+}
