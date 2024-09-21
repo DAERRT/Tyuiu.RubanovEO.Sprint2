9a08978212ac3a971bb7c5438ca8f94da7b14f56
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-09-21 21:54:00 +0500

    Task4

--- Tyuiu.RubanovEO.Sprint2.Task4.V16.Lib/DataService.cs
+++ Tyuiu.RubanovEO.Sprint2.Task4.V16.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.RubanovEO.Sprint2.Task4.V16.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = (x * 2) < (y * 2) ? Math.Pow(1 + (1 / Math.Pow(y, 2)), 2) : (y - (1 / Math.Pow(x, 2)));
+            double z = (x * 2) < (y * 2) ? Math.Pow(1 + (1 / Math.Pow(y, 2)), x) : (y - (1 / Math.Pow(x, 2)));
             return Math.Round(z, 3);
         }
     }
